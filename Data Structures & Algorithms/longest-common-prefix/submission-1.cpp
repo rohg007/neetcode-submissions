ccbece5abb69e25c74cf027560c1ca3fa4da5356
Rohan Gupta <32675945+rohg007@users.noreply.github.com>
2026-07-11 12:30:34 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int computeLCP(string s1, string s2) {
+        int i = 0, j = 0;
+        if (s1[0] != s2[0]) return -1;
+        while (i + 1 < s1.size() && j + 1 < s2.size() && s1[i + 1] == s2[j + 1]) {
+            ++i, ++j;
+        }
+        return i;
+    }
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string smallest = "";
+        int lcp = INT_MAX;
+        for (string s : strs) {
+            if (smallest == "" || s.size() < smallest.size())
+                smallest = s;
+        }
+
+        for (string s : strs) {
+            lcp = min(lcp, computeLCP(s, smallest));
+        }
+        if (lcp == -1) return "";
+        return smallest.substr(0, lcp + 1);
+    }
+};
